9eea9aa3b083ff0488a6b18c133816b18bcdda58
Sam~Lubbers <lubbers.sc@gmail.com>
2019-02-12 09:59:42 +0000

    remove datasets

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 DATASETS_PATH = os.path.join(ROOT_PATH,'datasets')
+ASSETS_PATH = os.path.join(ROOT_PATH, 'assets')
+
+COLOURS = {
+	'blue': '#4285F4'
+}
