a07638e0be6187e96c7f257d9f23c83a810089ad
Sam~Lubbers <lubbers.sc@gmail.com>
2019-04-10 17:50:15 +0100

    load all training images into variable

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 DATASETS_PATH = os.path.join(ROOT_PATH,'datasets')
 ASSETS_PATH = os.path.join(ROOT_PATH, 'assets')
+TRAIN_PATH = os.path.join(DATASETS_PATH, 'train')
+VALIDATION_PATH = os.path.join(DATASETS_PATH, 'validation')
 
 COLOURS = {
 	'blue': '#4285F4',
--- helpers/utils.py
+++ helpers/utils.py
@@ -5,15 +5,14 @@ from spectral import imshow, get_rgb
 
 import sys
 sys.path.append("../")
-from config import DATASETS_PATH
-
-TRAIN_PATH = os.path.join(DATASETS_PATH, 'train')
+from config import TRAIN_PATH
 
 def load_image(file_name, imgs_path=TRAIN_PATH):
     path = os.path.join(imgs_path, file_name)
     return io.imread(path)
 
 def tif_to_rgb(img):
+    """returns image with rgb values between 0 and 1"""
     # [2, 1, 0] index is needed because colours are stored as bgr
     return get_rgb(img, [2,1,0]) 
 
