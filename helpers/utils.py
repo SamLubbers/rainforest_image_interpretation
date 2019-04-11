128e579250587f6e2d3dfa99392e9f249760a937
Sam~Lubbers <lubbers.sc@gmail.com>
2019-04-11 11:48:10 +0100

    reorder labels to match data fix

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
-from skimage import io
-from spectral import imshow, get_rgb
+from skimage.io import imread, imshow
+from spectral import get_rgb
 
 import sys
 sys.path.append("../")
@@ -9,14 +9,14 @@ from config import TRAIN_PATH
 
 def load_image(file_name, imgs_path=TRAIN_PATH):
     path = os.path.join(imgs_path, file_name)
-    return io.imread(path)
+    return imread(path)
 
 def tif_to_rgb(img):
     """returns image with rgb values between 0 and 1"""
-    # [2, 1, 0] index is needed because colours are stored as bgr
+    # [2, 1, 0] index is needed because colours are stored as bgr (wavelength order)
     return get_rgb(img, [2,1,0]) 
 
-def show_tif_img(file_name, imgs_path=TRAIN_PATH):
+def load_and_show_img(file_name, imgs_path=TRAIN_PATH):
     img = load_image(file_name, imgs_path)
     rgb = tif_to_rgb(img)
     imshow(rgb)
